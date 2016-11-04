febf897c5359cc54c85218b0a6fc1256fc1d5f03
anthonylgrant <anthonylorenzogrant@gmail.com>
2016-11-04 20:22:45 +0000

    added chalk variables and color changes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var chalk = require("chalk");
+const chalk = require("chalk");
 
-var message = "Hello " + chalk.purple("World");
+
+var redbold = chalk.red.bold;
+var bluebold = chalk.blue.bold;
+var yellow = chalk.yellow
+var yellowbold = chalk.yellow.bold;
+
+var message = "Hello " + bluebold("Blue Hello") + " " + yellowbold("Yellow Bold");
 console.log(message);
